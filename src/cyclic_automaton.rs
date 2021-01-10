9f6ed23e9ac395a2a948780e6d257a603f5df3e5
AlperenAydin <aydin.alperen.bugra@gmail.com>
2021-01-10 10:51:40 +0100

    Changed how the new generation get calculated. No longer uses (x,y) but the index directly. Added some tests for get_neighbour

--- src/cyclic_automaton.rs
+++ src/cyclic_automaton.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use rand::{distributions::Uniform, rngs::StdRng, Rng, SeedableRng};
 use std::ops::{Index, IndexMut};
 
@@ -7,26 +8,36 @@ use std::ops::{Index, IndexMut};
 // If the number of its neighbours whose state is n+1
 // is greater than threshold, the cell is consumed
 // And its state becomes n+1
-// Remember that we are operating in module space. 
+// Remember that we are operating in module space.
 // Meaning that its cycles back. num_states + 1 -> 0
+// i = x + y*width
+// (0,0), (1,0)
+// (0,1), (1,1)
+// => [(0,0), (1,0), (0,1), (1,1)]
 pub struct CyclicAutomaton {
     pub grid: Vec<u8>,
     pub width: usize,
     pub height: usize,
     pub num_states: u8,
     pub threshold: usize,
+    pub neighbours: Vec<[usize; 8]>,
 }
 
 impl CyclicAutomaton {
     // Creates new automaton, with the value initialized to 0.
     pub fn new(width: usize, height: usize, num_states: u8, threshold: usize) -> CyclicAutomaton {
-        CyclicAutomaton {
+        let mut new_automaton = CyclicAutomaton {
             grid: vec![0; width * height],
             width,
             height,
             num_states,
             threshold,
+            neighbours: vec![[0 as usize; 8]; width * height],
+        };
+        for index in 0..(width * height) {
+            new_automaton.neighbours[index] = CyclicAutomaton::get_neighbours(index, width, height);
         }
+        new_automaton
     }
     // Randomizes the grid from a seed
     pub fn randomize(&mut self, seed: u64) {
@@ -35,51 +46,52 @@ impl CyclicAutomaton {
         let size = self.width * self.height;
         self.grid = rng.sample_iter(&dist).take(size).collect();
     }
-    // Return all 8 of the cell located at (x,y)
-    fn neighbours(x: usize, y: usize) -> [(i32, i32); 8] {
-        let mut cells = [(0 as i32, 0 as i32); 8];
-        let (x, y) = (x as i32, y as i32);
-        // Starting on the left, going clockwise
-        cells[0] = (x - 1, y);
-        cells[1] = (x - 1, y + 1);
-        cells[2] = (x, y + 1);
-        cells[3] = (x + 1, y + 1);
-        cells[4] = (x + 1, y);
-        cells[5] = (x + 1, y - 1);
-        cells[6] = (x, y - 1);
-        cells[7] = (x - 1, y - 1);
-        cells
+    // Return all 8 neighbours of the cell located at index
+    fn get_neighbours(index: usize, width: usize, height: usize) -> [usize; 8] {
+        // The neighbours of (x,y) is (x+a, x+b)
+        // st. for (a,b) in [-1,0,1]**2 except (0,0)*
+        let x = (index % width) as i32;
+        let y = (index / width) as i32;
+        let mask2coords = |(a, b): (i32, i32)| {
+            (
+                (x + a).rem_euclid(width as i32),
+                (y + b).rem_euclid(height as i32),
+            )
+        };
+        let neighbours = iproduct!(-1..=1, -1..=1) // Mask for each direction
+            .filter(|&p| p != (0, 0)) // Removes the center
+            .map(mask2coords) // Turns the mask into 2d coordinates
+            .map(|(a, b)| (a as usize, b as usize))
+            .map(|(a, b)| a + (b * width)); // Turns coords into index
+        let mut ret: [usize; 8] = [0; 8]; // The return value
+        // Filling in the return value
+        for (i, val) in neighbours.enumerate() {
+            ret[i] = val;
+        }
+        ret
     }
     // Updates the cell at (x,y)
     // According to the rue explained above
-    pub fn update_cell(&self, x: usize, y: usize) -> u8 {
-        let next_state = (self[(x, y)] + 1) % self.num_states;
-        let count = CyclicAutomaton::neighbours(x, y)
+    pub fn update_cell(&self, index: usize) -> u8 {
+        let curr_state = self.grid[index];
+        let next_state = (curr_state + 1) % self.num_states;
+        let neighbour_count = self.neighbours[index]
             .iter()
-            .filter(|(n_x, n_y)| self[(*n_x, *n_y)] == next_state).
-            count();
-
-        if count > self.threshold{
+            .filter(|&i| self.grid[*i] == next_state)
+            .count();
+        if neighbour_count > self.threshold {
             return next_state;
-        } else {
-            return self[(x,y)];
         }
+        curr_state
     }
 
-    pub fn next_generation(&mut self)
-    {
-        let mut new_grid = vec![0; self.width*self.height];
-        for x in 0 .. self.width
-        {
-            for y in 0..self.height{
-                let i = x + y * self.width;
-                new_grid[i]= self.update_cell(x, y);
-            }
-        }
-        self.grid = new_grid;
+    pub fn next_generation(&mut self) {
+        let index_range = 0..(self.width * self.height);
+        self.grid = index_range.map(|index| self.update_cell(index)).collect();
     }
 }
 
+// This was previously useful. But I will live it in just in case.
 impl Index<(i32, i32)> for CyclicAutomaton {
     type Output = u8;
     fn index(&self, (x, y): (i32, i32)) -> &Self::Output {
@@ -103,5 +115,23 @@ impl IndexMut<(usize, usize)> for CyclicAutomaton {
         let x = (x as usize).rem_euclid(self.width);
         let y = (y as usize).rem_euclid(self.height);
         &mut self.grid[x + y * self.width]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // TODO: Write more and better tests
+
+    #[test]
+    fn test_get_neighbours() {
+        let ground_truth: [usize; 8] = [99, 399, 699, 100, 700, 101, 401, 701];
+        let neighbours = CyclicAutomaton::get_neighbours(400, 300, 300);
+        assert_eq!(neighbours, ground_truth);
+        //assert_eq!(0,1);
+
+        let ground_truth: [usize; 8] = [149899, 399, 899, 149900, 900, 149901, 401, 901];
+        let neighbours = CyclicAutomaton::get_neighbours(400, 500, 300);
+        assert_eq!(neighbours, ground_truth);
     }
 }
